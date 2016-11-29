2fdd34f778b2ee6f8b66054eeefeb1241890138f
Mirko Zanecchia <mirko.z@improntaadv.com>
2016-11-29 18:57:03 +0100

    Added inline queries logic

--- MKMBot/requests.js
+++ MKMBot/requests.js
@@ -26,6 +26,35 @@ function getCardPrice(card, priceType) {
     });
 }
 
+function getMkmCardLink(card) {
+    return new Promise(function(resolve, reject) {
+        request(`${ENDPOINT}/price?name=${card.trim()}`, function(err, res, body) {
+            if(err)
+                return reject(err);
+
+            if(res.statusCode === 404) {
+                return reject({
+                    message: 'Oops, I cannot found  the card. Are you sure name e/or syntax are correct?',
+                });
+            }
+
+            try {
+                var cards = JSON.parse(body).splice(0, 4).map(function(el) {
+                    return {
+                        link: `https://it.magiccardmarket.eu/Products/Singles/${encodeURIComponent(el.expansionMkm)}/${encodeURIComponent(el.names[0])}`,
+                        name: el.names[0],
+                        expansion: el.expansion,
+                    };
+                });
+                resolve(cards);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    });
+}
+
 module.exports = {
     getCardPrice,
+    getMkmCardLink,
 };
