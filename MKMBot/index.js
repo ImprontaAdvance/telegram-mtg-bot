b9ab25edef948f4317d52e24d0e97e916fb51ab6
Mirko Zanecchia <mirko.z@improntaadv.com>
2016-12-05 11:46:33 +0100

    Change price types visualization and delete /low command

--- MKMBot/index.js
+++ MKMBot/index.js
@@ -21,17 +21,6 @@ bot.onText(/^\/price (.+)$/, function(msg, match) {
         });
 });
 
-bot.onText(/^\/low (.+)$/, function(msg, match) {
-    commands.low(msg, match)
-        .then(res => bot.sendMessage(msg.chat.id, res.text, res.options))
-        .catch(e => {
-            console.error(e);
-
-            if(e.message)
-                bot.sendMessage(msg.chat.id, e.message);
-        });
-});
-
 
 bot.onText(/^\/start$/, function(msg, match) {
     var start = commands.start(msg, match).then(start => {
@@ -58,15 +47,15 @@ bot.onText(/^\/credits$/, function(msg, match) {
     });
 });
 
-bot.onText(/^\/(price|low)$/, function(msg, match) {
+bot.onText(/^\/price$/, function(msg, match) {
     bot.sendMessage(msg.chat.id, 'Please provide one or more card names. I can\'t read your mind. Yet.');
 });
 
-bot.onText(/.+\/(start|help|credits|price|last|low)/, function(msg, match) {
+bot.onText(/.+\/(start|help|credits|price|last)/, function(msg, match) {
     bot.sendMessage(msg.chat.id, 'Don\'t type text before a command, please.');
 });
 
-bot.onText(/^\/(?!credits|help|start|price|last|low).*$/, function(msg, match) {
+bot.onText(/^\/(?!credits|help|start|price|last).*$/, function(msg, match) {
     bot.sendMessage(msg.chat.id, 'I don\'t know this command. If you\'re lost, try /help.');
 });
 
